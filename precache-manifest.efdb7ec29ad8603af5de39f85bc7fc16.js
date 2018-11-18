1b237e806660ce3c8809b53b337f62ea26a9597e
oldworm <oldworm2@gmail.com>
2018-11-18 22:06:44 +1300

    Updates

---
+++ precache-manifest.efdb7ec29ad8603af5de39f85bc7fc16.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8d376b22c7dc1784f27c",
+    "url": "/sushi-calculator/static/js/runtime~main.8d376b22.js"
+  },
+  {
+    "revision": "af8e99c8e0ef46dd1850",
+    "url": "/sushi-calculator/static/js/main.af8e99c8.chunk.js"
+  },
+  {
+    "revision": "f60f12129410a8e3ecfa",
+    "url": "/sushi-calculator/static/js/1.f60f1212.chunk.js"
+  },
+  {
+    "revision": "af8e99c8e0ef46dd1850",
+    "url": "/sushi-calculator/static/css/main.33785ff2.chunk.css"
+  },
+  {
+    "revision": "5b6726a4c47b2225aa2768e60bfb3679",
+    "url": "/sushi-calculator/index.html"
+  }
+];
